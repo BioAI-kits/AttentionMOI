bcc39e06edeff3991e2e489caf53cc6ba654a4fe
pangjli <74547628+pangjli@users.noreply.github.com>
2022-10-27 10:01:27 +0800

    Add files via upload

--- demo.sh
+++ demo.sh
@@ -1,1 +1,8 @@
-python deepmoi.py -f ./dataset/Test/rna.csv.gz -f ./dataset/Test/cnv.csv.gz -f ./dataset/Test/met.csv.gz  -l ./dataset/Test/label.csv -n rna -n cnv -n met -s 42 -b 16 --threshold 0.6
+mkdir ./external_out
+touch ./external_out/evaluation.txt
+echo -e "model\tfeature_selection\tACC\tPrecision\tF1_score\tAUC\tRecall"  > ./external_out/evaluation.txt
+python deepmoi.py -f ./dataset/Test/rna.csv.gz -f ./dataset/Test/cnv.csv.gz -f ./dataset/Test/met.csv.gz  -l ./dataset/Test/label.csv -n rna -n cnv -n met --FSD --method RFE -m all -s 42 -b 16 --threshold 0.6 -o ./external_out
+
+touch ./external_out/evaluation.txt
+echo -e "model\tfeature_selection\tACC\tPrecision\tF1_score\tAUC\tRecall"  > ./external_out/evaluation_external.txt
+python deepmoi_prediction.py -f ./dataset/Test/rna.csv.gz -f ./dataset/Test/cnv.csv.gz -f ./dataset/Test/met.csv.gz  -l ./dataset/Test/label.csv -n rna -n cnv -n met --FSD --method RFE -m all -s 42 -o ./external_out
