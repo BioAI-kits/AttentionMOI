f358795e72e1888a53d484dbc6fbcea9b29cb60b
Billy <liangbilin0324@126.com>
2023-04-12 19:02:43 +0800

    updatte

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ from setuptools import setup, find_packages
 
 
 install_packages = [
-    'captum==0.4.1',
-    'mygene==3.2.2',
-    'openpyxl==3.0.9',
-    'packaging==21.3',
-    'pandas==1.2.5',
-    'pandocfilters==1.5.0',
-    'seaborn==0.11.2',
+    'captum>=0.4.1',
+    'mygene>=3.2.2',
+    'openpyxl>=3.0.9',
+    'packaging>=21.3',
+    'pandas>=1.2.5',
+    'pandocfilters>=1.5.0',
+    'seaborn>=0.11.2',
     'torch==1.13.1',
-    'scikit-learn==1.2.2',
-    'numpy==1.23.5',
-    'matplotlib==3.6.2',
-    'xgboost==1.7.4',
+    'scikit-learn>=1.2.2',
+    'numpy>=1.23.5',
+    'matplotlib>=3.6.2',
+    'xgboost>=1.7.4',
     'livelossplot', 
     'tensorboardX',
     'tqdm',
@@ -28,7 +28,7 @@ setup(
     # 作者邮箱
     author_email='liangbilin0324@163.com',
     # 版本号
-    version='0.1.1',
+    version='0.1.2',
     # 要求python版本
     python_requires=">=3.9.*",
     # 找到本目录下的所有python包
